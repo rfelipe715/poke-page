7e40bb45671d51b2abd697fb35b1d8b5078d0e23
Felipe <fe.rioso@duocuc.cl>
2026-09-09 18:28:14 -0300

    Implement logout button in header and style it

--- JS/js.js
+++ JS/js.js
@@ -80,6 +80,7 @@ if (tipoURL) {
 
 const usuarioHeader = document.getElementById('usuario-header');
 const nombreHeader = document.getElementById('nombre-header');
+const botonSalirHeader = document.getElementById('btn-cerrar-sesion-header');
 
 const nombreUsuario = localStorage.getItem('nombre_usuario');
 
@@ -90,9 +91,24 @@ if (nombreUsuario) {
     // Si ya inició sesión, el monito manda a la cuenta
     usuarioHeader.href = 'mi-cuenta.html';
 
+    botonSalirHeader.hidden = false;
+
 } else {
 
     // Si no inició sesión, manda al login
     usuarioHeader.href = 'login.html';
 
+    botonSalirHeader.hidden = true;
+
 }
+
+// Cerrar sesión desde el header
+botonSalirHeader.addEventListener('click', () => {
+
+    localStorage.removeItem('nombre_usuario');
+    localStorage.removeItem('email_usuario');
+    localStorage.removeItem('rol_usuario');
+
+    window.location.href = 'index.html';
+
+});
